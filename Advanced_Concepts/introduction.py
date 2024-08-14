7f38bb3cc1597b8fcb32dd2a802abc53a66b2d73
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2024-08-14 21:06:54 +0300

    2.01

--- Advanced_Concepts/introduction.py
+++ Advanced_Concepts/introduction.py
@@ -38,3 +38,50 @@ st.cache_resource -> anything you CAN'T store in a database  -- Ml models; datab
 """
 
 
+"""
+## Session State
+
+Session State provides a dictionary-like interface where we can save information that is preserved between script reruns. 
+Use st.session_state with key or attribute notation to store and recall values. For example, st.session_state["m_key] or st.session_state.my_key. 
+Remember that widgets handle their statefulness all by themselves, so you won't always need to use Session State!
+
+### What is a session? 
+A session is a single instance of viewing an app. If you view an app from two different tabs in your browser, each tab will have its own session. So each viewer of an app will have a Session State tied to their specific view. 
+Streamlit maintains this session as the user interacts with the app. If the user refreshes their browser page or reloads the URL to the app,
+their Session State resets and they begin again with a new session.
+"""
+if "counter" not in st.session_state:
+    st.session_state.counter = 0 
+st.session_state.counter += 1 
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
+
+"""
+There are a few common scenarios where Session State is helpful. As demonstrated above, Session State is used when you have a progressive process that you want to build upon from one rerun to the next. Session State can also be used to prevent recalculation, similar to caching. 
+However, the differences are important:
+- Caching associates stored values to specific functions and inputs. Cached values are accessible to all users across all sessions.
+- Session State associates stored values to keys (strings). Values in session state are only available in the single session where it was saved.
+
+
+If you have random number generation in your app, you'd likely use Session State. Here's an example where data is generated randomly at the beginning of each session. By saving this random information in Session State,
+ each user gets different random data when they open the app but it won't keep changing on them as they interact with it. 
+If you select different colors with the picker you'll see that the data does not get re-randomized with each rerun. (If you open the app in a new tab to start a new session, you'll see different data!)
+"""
+import pandas as pd 
+import numpy as np 
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(np.random.randn(20,2), columns=["x", "y"])
+
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
+
+
+"""
+If you are pulling the same data for all users, you would likely cache a function that retrieves that data.
+On the other hand, if we pull data specific to a user, such as querying their personal information, we may want to save that in Session State.
+That way, the queried data is only available in that one session.
+"""
